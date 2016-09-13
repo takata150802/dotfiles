3917e2839a9cf15063a38d0dee22fbdc2e5db66f
takata <takata@hal.ipc.i.u-tokyo.ac.jp>
2016-09-13 18:08:57 +0900

    fix create_link.sh and .conkyrc*

--- create_link.sh
+++ create_link.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash -eu
 cd $(dirname $0)
+if [ -e .conkyrc ]; then
+    rm .conkyrc
+else
+    :
+fi
+
+if [ $HOSTNAME = "takata-hal-lab01" ]; then
+    ln -s .conkyrc_hal-lab01 .conkyrc
+elif [ $HOSTNAME = "cognitive0"  ]; then
+    ln -s .conkyrc_cognitive0 .conkyrc
+else 
+    echo "Please register your host name & .conkyrc!"
+fi
+
 for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
